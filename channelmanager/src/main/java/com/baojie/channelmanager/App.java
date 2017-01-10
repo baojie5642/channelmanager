154960f1fbf57dddb80226934be1cbf9cb3ffc14
baojie <baojie@baojie-PC.lan>
2017-01-10 14:17:22 +0800

    Initial Commit

---
+++ channelmanager/src/main/java/com/baojie/channelmanager/App.java
@@ -0,0 +1,13 @@
+package com.baojie.channelmanager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
